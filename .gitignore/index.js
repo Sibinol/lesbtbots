a7cd2176a171b6a3f5ce7496fd2c1df72007e643
Sibinol <50032552+Sibinol@users.noreply.github.com>
2019-04-27 17:43:29 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
+
+bot.login('NTcxNzE1NDc2NTgxNzc3NDA4.XMRx-Q.wP36zjpba0-GVxmvkgB80nbAgXo')
